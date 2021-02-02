15bb379897ef260890a66266e1400dff6d85f22a
Shairyar Baig <shairyarbaig@gmail.com>
2021-02-02 15:24:29 +0500

    Add custom instrumentation for a plug

--- lib/appsignal_test/handler.ex
+++ lib/appsignal_test/handler.ex
@@ -1,13 +1,15 @@
 defmodule Handler do
-  use Appsignal.Plug
 
   import Plug.Conn
 
   def init(opts), do: opts
 
   def call(conn, _opts) do
-    conn
-    |> put_resp_content_type("text/plain")
-    |> send_resp(200, "Hello world")
+    Appsignal.instrument "AppsignalTest.Plug.Home", fn ->
+      Process.sleep(1000)
+      conn
+      |> put_resp_content_type("text/plain")
+      |> send_resp(200, "Hello there two")
+    end
   end
 end
